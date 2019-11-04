da52d9d0a5d6bc55f4686a458d30189a21e009b9
noronhasandrew <noronhasandrew@gmail.com>
2019-11-03 22:44:55 -0300

    Fix validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,41 +5,41 @@ const nullEmpty = (field) => {
 }
 
 const testLogin = (login) => {
-    if (nullEmpty(login) || !(/^.{6,20}$/.test(login)))
-        return { result: false, msg: 'Login inválido' };
-    return { result: true };
+    if (!nullEmpty(login) && /^.{6,20}$/.test(login))
+        return { result: true };
+    return { result: false, msg: 'Login inválido' };
 }
 
 
 const testName = (name) => {
-    if (nullEmpty(name) || /[^a-zà-ú]/gi.test(name))
-        return { result: false, msg: 'Nome inválido' };
-    return { result: true };
+    if (!nullEmpty(name) && /[^a-zà-ú]/gi.test(name))
+        return { result: true };
+    return { result: false, msg: 'Nome inválido' };
 }
 
-const testPassword = (password, min, max) => {
-    if (nullEmpty(password) || /^\s+$/.test(password) || password.length < min || password.length > max)
-        return { result: false, msg: `Senha inválida. Tamanho mínimo de ${min} e máximo de ${max}` };
-    return { result: true };
+const testPassword = (password) => {
+    if (!nullEmpty(password) && /^.{6,20}$/.test(password))
+        return { result: true };
+    return { result: false, msg: 'Senha inválida' };
 }
 
 const testEmail = (email) => {
-    if (nullEmpty(email))
-        return { result: false, msg: 'Email inválido' };
-    return { result: true };
+    if (!nullEmpty(email) && /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi.test(email))
+        return { result: true };
+    return { result: false }
 }
 
 const testAdress = (adress) => {
-    if (nullEmpty(adress))
-        return { result: false, msg: 'Endereço inválido' };
-    return { result: true };
+    if (!nullEmpty(adress) && /^.{5,20}$/.test(adress))
+        return { result: true };
+    return { result: false, msg: 'Endereço inválido' };
 }
 
 exports.user = (user) => {
 
     const errors = []; //Erros dos inputs
 
-    let validPassword = testPassword(user.getPassword(), 6, 20);
+    let validPassword = testPassword(user.getPassword());
     let validEmail = testEmail(user.getEmail());
     let validName = testName(user.getName());
     let validLogin = testLogin(user.getLogin())
